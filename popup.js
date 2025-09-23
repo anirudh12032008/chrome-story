6e24087c64d1dbc2b0282fc6465f3500da740e14
Anirudh Sahu <renudilip19@gmail.com>
2025-09-23 09:47:01 +0530

    fixed the url text issue to show only host name added better styling to img to show full img increased the overall popup size cause it was small lol

--- popup.js
+++ popup.js
@@ -58,10 +58,12 @@ let count = 0
 function addcard(img, title, url, time) {
     count++;
     const x = document.createElement('div');
-    x.className = "flex flex-col items-center bg-gray-700 text-white p-3 my-2 rounded-xl shadow-xl hover:scale-107 transform transition-all duration-500 w-full";
+    // x.className = "flex flex-col items-center bg-gray-700 text-white p-3 my-4 rounded-xl sm:flex-row shadow-xl hover:scale-107 transform transition-all duration-500 w-full";
+    x.className = "flex flex-col items-center bg-gray-700 text-white p-3 my-4 rounded-xl  shadow-xl hover:scale-107 transform transition-all duration-500 w-full";
     const image = document.createElement('img');
     image.src = img;
-    image.className = "w-full h-40 object-cover rounded-t-xl shadow-xl";
+    image.className = "w-full h-auto object-contain rounded-t-xl shadow-xl";
+    image.style.maxHeight = "300px";
    
     // i was making a mistake here so I am just making a new div for everything
     // const dets = document.createElement('div');
@@ -72,13 +74,13 @@ function addcard(img, title, url, time) {
     // no.textContent = `#${count} - ${new Date(time).toLocaleString()}`;
 
     const dets = document.createElement('div');
-    dets.className = "flex flex-col justify-between items-start p-2 space-y-1";
+    dets.className = "flex flex-col justify-between w-full px-5 items-start p-2 space-y-1";
 
     const no = document.createElement('div');
     no.className = "text-sm text-gray-300";
     no.textContent = `#${count}`;
 const t = document.createElement('div');
-t.className = "font-semibold truncate";
+t.className = "font-semibold";
     t.innerText = title;
     const u = document.createElement('a');
     u.className = "text-blue-400 text-sm truncate";
@@ -86,7 +88,13 @@ t.className = "font-semibold truncate";
     u.target = "_blank";
     // this was causing issues due to google search having too big urls
     // u.innerText = url;
-    
+    try {
+        const p = new URL(url);
+        u.innerText = p.hostname;
+    } catch (e) {
+    //  :prayge: hope this never happens 
+        u.innerText = url;
+    }
     const ti = document.createElement('div');
     ti.className = "text-xs text-gray-400";
     ti.innerText = new Date(time).toLocaleString();
